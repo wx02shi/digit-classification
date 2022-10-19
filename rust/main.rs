b5e856bc26593277e723f4c206268bb77e032933
William Shi <33271663+wx02shi@users.noreply.github.com>
2022-10-19 01:58:28 -0400

    install rust, create and run a main

---
+++ rust/main.rs
@@ -0,0 +1,9 @@
+/**
+     * It's 1:43 AM, I've spent an eternity
+     * recreating and understanding the Python
+     * implementation of digit classification.
+     * It's time I go to sleep.
+     */
+fn main() {
+    println!("This is a work in progress.");
+}
